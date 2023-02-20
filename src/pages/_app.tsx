10c53718919be25bdfdfa0051b1d4e94f8ba64b8
tooooofu24 <toya24xxx@i.softbank.jp>
2023-02-20 20:31:47 +0900

    ライトモードに変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GeistProvider themeType="dark">
+    <GeistProvider themeType="light">
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
